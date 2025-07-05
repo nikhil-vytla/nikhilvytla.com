62b24ceee345c0ab4fdfed134d17c373a395a333
Nikhil Vytla <cnikhil.vytla@gmail.com>
2025-07-04 19:38:08 -0700

    feat: add build and utility scripts

---
+++ scripts/rss.ts
@@ -0,0 +1,88 @@
+import type { FeedOptions, Item } from 'feed'
+import { dirname } from 'node:path'
+import fg from 'fast-glob'
+import { Feed } from 'feed'
+import fs from 'fs-extra'
+import matter from 'gray-matter'
+import MarkdownIt from 'markdown-it'
+
+const DOMAIN = 'https://nikhilvytla.com'
+const AUTHOR = {
+  name: 'Nikhil Vytla',
+  email: 'nikhil@nikhilvytla.com',
+  link: DOMAIN,
+}
+const markdown = MarkdownIt({
+  html: true,
+  breaks: true,
+  linkify: true,
+})
+
+async function run() {
+  await buildBlogRSS()
+}
+
+async function buildBlogRSS() {
+  const files = await fg('pages/posts/*.md')
+
+  const options = {
+    title: 'Nikhil Vytla',
+    description: 'Nikhil Vytla\'s Blog',
+    id: 'https://nikhilvytla.com/',
+    link: 'https://nikhilvytla.com/',
+    copyright: 'CC BY-NC-SA 4.0 2025 - PRESENT © Nikhil Vytla',
+    feedLinks: {
+      json: 'https://nikhilvytla.com/feed.json',
+      atom: 'https://nikhilvytla.com/feed.atom',
+      rss: 'https://nikhilvytla.com/feed.xml',
+    },
+  }
+  const posts: any[] = (
+    await Promise.all(
+      files.filter(i => !i.includes('index'))
+        .map(async (i) => {
+          const raw = await fs.readFile(i, 'utf-8')
+          const { data, content } = matter(raw)
+
+          if (data.lang !== 'en')
+            return
+
+          const html = markdown.render(content)
+            .replace('src="/', `src="${DOMAIN}/`)
+
+          if (data.image?.startsWith('/'))
+            data.image = DOMAIN + data.image
+
+          return {
+            ...data,
+            date: new Date(data.date),
+            content: html,
+            author: [AUTHOR],
+            link: DOMAIN + i.replace(/^pages(.+)\.md$/, '$1'),
+          }
+        }),
+    ))
+    .filter(Boolean)
+
+  posts.sort((a, b) => +new Date(b.date) - +new Date(a.date))
+
+  await writeFeed('feed', options, posts)
+}
+
+async function writeFeed(name: string, options: FeedOptions, items: Item[]) {
+  options.author = AUTHOR
+  options.image = 'https://nikhilvytla.com/avatar.png'
+  options.favicon = 'https://nikhilvytla.com/logo.png'
+
+  const feed = new Feed(options)
+
+  items.forEach(item => feed.addItem(item))
+  // items.forEach(i=> console.log(i.title, i.date))
+
+  await fs.ensureDir(dirname(`./dist/${name}`))
+  await fs.writeFile(`./dist/${name}.xml`, feed.rss2(), 'utf-8')
+  await fs.writeFile(`./dist/${name}.atom`, feed.atom1(), 'utf-8')
+  await fs.writeFile(`./dist/${name}.json`, feed.json1(), 'utf-8')
+}
+
+run()
